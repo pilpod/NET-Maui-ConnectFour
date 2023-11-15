6b10ceefd02763e1f3897ceadefc2aae28382b3f
Giacomo <giacomo.corso@gmail.com>
2023-11-15 11:18:18 +0100

    init repo

---
+++ ConnectFour/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ConnectFour
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ConnectFour/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ConnectFour
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
