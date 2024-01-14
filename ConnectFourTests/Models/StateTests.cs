905f0b4c9748a5d4f70031956a9c06b694834e34
Giacomo <giacomo.corso@gmail.com>
2024-01-14 16:21:50 +0100

    test State Can Reset the game done

--- ConnectFourTests/Models/StateTests.cs
+++ ConnectFourTests/Models/StateTests.cs
@@ -15,13 +15,15 @@ namespace ConnectFourTests.Models
             state.players[0].Points = 2;
             state.players[1].Points = 3;
             
+            state.PlayPiece(0);
             state.ResetGame();
 
             Assert.False(state.GameOver);
             Assert.Equal(0, state.players[0].Points);
             Assert.Equal(0, state.players[1].Points);
-            Assert.Collection(state.pieces,
-                piece => Assert.Null(piece));
+            Assert.All(state.pieces, piece => Assert.Null(piece));
+
+            //Assert.Collection(state.pieces, piece => Assert.Null(piece));
         }
 
     }
